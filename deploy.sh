70366aef27a76ecdbedf0b53f2ed79ddcfc8f419
WosAlan <806452789@qq.com>
2021-06-10 19:11:18 +0800

    更新了样式

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git commit -m '更新了样式'
 # 如果你想要部署到 coding上
 # .ssh -> config -> nodeing666 对应e.coding.net
 # git@e.coding.net:wosalan/vuepressblog/blog.git
-git push -f git@alan666:Alan-26/CS_Review.git master
+git push -f git@github.com:Alan-26/CS_Review.git master
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
 cd -
