9cd65de32fc48ab1cafb5cd30003fc8d2d5fe4b5
WosAlan <806452789@qq.com>
2021-06-10 19:04:21 +0800

    更新了样式

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# 如果是发布到自定义域名
+echo 'cs-review.cn' > CNAME
+
+git init
+git config user.name "WosAlan"
+git config user.email "806452789@qq.com"
+git add -A
+git commit -m '更新了样式'
+
+# 如果你想要部署到 coding上
+# .ssh -> config -> nodeing666 对应e.coding.net
+# git@e.coding.net:wosalan/vuepressblog/blog.git
+git push -f git@alan666:Alan-26/CS_Review.git master
